eca3e42180484abb6fb30919a36acec5c5a5b444
batallan <batallanmaximo03@gmail.com>
2023-05-22 10:54:27 -0300

    Ejercicio 1

---
+++ practico6/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+Console.WriteLine("Hello, World!");
+
+int a;
+int b;
+
+a=10;
+b=a;
+
+Console.WriteLine("Valor de a: "+a);
+Console.WriteLine("Valor de b: "+b);
+*/
+
+//Ejercicio 1
+
+int num, dec, cen, uni, nuevoNum;
+
+Console.WriteLine("Ingrese un numero de tres cifras: ");
+bool control=int.TryParse(Console.ReadLine(), out num);
+
+if(control){
+
+    dec=num/10;
+    dec=dec%10;
+    cen=num/100;
+    uni=num%10;
+
+    nuevoNum=(uni*100)+(dec*10)+cen;
+
+    Console.WriteLine("Numero Invertido: "+nuevoNum);
+
+}else{
+    Console.WriteLine("No es un numero, Ingrese de nuevo");
+}
